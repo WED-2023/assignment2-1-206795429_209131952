5c79d81c9233249cf5aead4d044ddf4f7d25ea8f
SapirTzaig <tzaigsap@post.bgu.ac.il>
2024-07-04 14:07:20 +0300

    state

--- src/services/user.js
+++ src/services/user.js
@@ -3,6 +3,7 @@ import recipe_full_view from "../assets/mocks/recipe_full_view.json";
 import recipe_preview from "../assets/mocks/recipe_preview.json";
 
 
+
   export function mockAddFavorite(recipeId) {
     return { status: 200, response: { data: { message: "The Recipe successfully saved as favorite", success: true}} };
   }
@@ -12,4 +13,7 @@ import recipe_preview from "../assets/mocks/recipe_preview.json";
 
   }
   
-  
+  const state = {
+    // server_domain: "http://localhost:3000",
+    server_domain: "https://FoodyShmoody.cs.bgu.ac.il"
+  }
